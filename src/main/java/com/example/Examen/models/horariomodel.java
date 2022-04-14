a0cca88ef7db606f2ab169d18e58f53e5196603d
MiguelOcotitla <angelocotitla1817@outlook.es>
2022-04-14 00:26:33 -0500

    Examen

---
+++ src/main/java/com/example/Examen/models/horariomodel.java
@@ -0,0 +1,56 @@
+package com.example.Examen.models;
+/*
+import java.sql.Time;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+//@Entity
+//@Table(name = "horario")*/
+public class horariomodel {
+  /*  
+    private int id;
+    private Time proye1;
+    private Time proye2;
+    private Time proye3;
+    private Time proye4;
+    private Time proye5;
+
+
+    
+    public int getId() {
+        return id;
+    }
+    public Time getProye5() {
+        return proye5;
+    }
+    public void setProye5(Time proye5) {
+        this.proye5 = proye5;
+    }
+    public Time getProye4() {
+        return proye4;
+    }
+    public void setProye4(Time proye4) {
+        this.proye4 = proye4;
+    }
+    public Time getProye3() {
+        return proye3;
+    }
+    public void setProye3(Time proye3) {
+        this.proye3 = proye3;
+    }
+    public Time getProye2() {
+        return proye2;
+    }
+    public void setProye2(Time proye2) {
+        this.proye2 = proye2;
+    }
+    public Time getProye1() {
+        return proye1;
+    }
+    public void setProye1(Time proye1) {
+        this.proye1 = proye1;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }*/
+}
